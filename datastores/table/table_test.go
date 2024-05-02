5cd3e89564ec99a2750fbb1e2625335b54583dab
ohzqq <iamchurkey@gmail.com>
2024-05-02 13:40:02 -0400

    new table tests are working

--- datastores/table/table_test.go
+++ datastores/table/table_test.go
@@ -1,20 +1,43 @@
 package table
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 )
 
 const hareTestDB = `testdata/hare`
 
 func TestNewTable(t *testing.T) {
-	table, err := File(hareTestDB, "index", ".json")
+	f, err := File(hareTestDB, "index", ".json")
 	if err != nil {
 		t.Error(err)
 	}
 
-	_, err = NewTable(table)
+	table, err := NewTable(f)
 	if err != nil {
 		t.Error(err)
 	}
-	//fmt.Printf("%#v\n", table.offsets)
+	want := 7251
+	if len(table.offsets) != want {
+		t.Errorf("got %v, wanted %v\n", len(table.offsets), want)
+	}
+}
+
+func TestMemTable(t *testing.T) {
+	d, err := os.ReadFile(filepath.Join(hareTestDB, "index.json"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	mem := Mem(d)
+
+	table, err := NewTable(mem)
+	if err != nil {
+		t.Error(err)
+	}
+	want := 7251
+	if len(table.offsets) != want {
+		t.Errorf("got %v, wanted %v\n", len(table.offsets), want)
+	}
 }
