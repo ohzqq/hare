ab2d1ad054956a48cda77db7349368f3713f2484
ohzqq <iamchurkey@gmail.com>
2024-05-03 11:02:13 -0400

    update disk

--- datastores/disk/testutils_test.go
+++ datastores/disk/testutils_test.go
@@ -1,4 +1,4 @@
-package disk
+package table
 
 import (
 	"os"
@@ -16,25 +16,56 @@ func runTestFns(t *testing.T, tests []func(t *testing.T)) {
 }
 
 func newTestDisk(t *testing.T) *Disk {
-	dsk, err := New("./testdata", ".json")
+	dsk, err := NewDisk("./testdata", ".json")
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("newTestDisk error %v\n", err)
 	}
 
 	return dsk
 }
 
-func newTestTableFile(t *testing.T) *tableFile {
+func newTestRam(t *testing.T) *Ram {
+	d, err := os.ReadFile("./testdata/contacts.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	tables := map[string][]byte{
+		"contacts": d,
+	}
+
+	ram, err := NewRam(tables)
+	if err != nil {
+		t.Fatalf("newTestRam error %v\n", err)
+	}
+	return ram
+}
+
+func newTestTableFile(t *testing.T) *Table {
 	filePtr, err := os.OpenFile("./testdata/contacts.json", os.O_RDWR, 0660)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("test new table open file error %v\n", err)
 	}
 
-	tf, err := newTableFile("contacts", filePtr)
+	tf, err := NewTable(filePtr)
+	if err != nil {
+		t.Fatalf("newTestTableFile new table error %v\n", err)
+	}
+
+	return tf
+}
+
+func newTestTableMem(t *testing.T) *Table {
+	d, err := os.ReadFile("./testdata/contacts.json")
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	mem := Mem(d)
+
+	tf, err := NewTable(mem)
+	if err != nil {
+		t.Fatal(err)
+	}
 	return tf
 }
 
@@ -43,7 +74,7 @@ func testSetup(t *testing.T) {
 
 	cmd := exec.Command("cp", "./testdata/contacts.bak", "./testdata/contacts.json")
 	if err := cmd.Run(); err != nil {
-		t.Fatal(err)
+		t.Fatalf("test cp error %v\n", err)
 	}
 }
 
@@ -57,7 +88,7 @@ func testRemoveFiles(t *testing.T) {
 	for _, f := range filesToRemove {
 		err := os.Remove("./testdata/" + f)
 		if err != nil && !os.IsNotExist(err) {
-			t.Fatal(err)
+			t.Fatalf("test rm files error %v\n", err)
 		}
 	}
 }
