f094526ff880963b527dc74aa6d435029e488cd9
ohzqq <iamchurkey@gmail.com>
2024-05-07 12:39:09 -0400

    close tables after open

--- database.go
+++ database.go
@@ -125,6 +125,7 @@ func (db *Database) Delete(tableName string, id int) error {
 	if err != nil {
 		return err
 	}
+	defer tbl.Close()
 
 	if err := tbl.Delete(id); err != nil {
 		return err
@@ -163,6 +164,7 @@ func (db *Database) Find(tableName string, id int, rec Record) error {
 	if err != nil {
 		return err
 	}
+	defer tbl.Close()
 
 	return tbl.Find(id, rec)
 }
@@ -174,6 +176,7 @@ func (db *Database) IDs(tableName string) ([]int, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer tbl.Close()
 
 	return tbl.IDs()
 }
@@ -186,6 +189,7 @@ func (db *Database) Insert(tableName string, rec Record) (int, error) {
 	if err != nil {
 		return 0, err
 	}
+	defer tbl.Close()
 
 	return tbl.Insert(rec)
 }
