492c5c1b31210c0de27dde033893d0867b415489
ohzqq <iamchurkey@gmail.com>
2024-05-08 11:54:11 -0400

    switch up changes

--- database.go
+++ database.go
@@ -3,6 +3,7 @@
 package hare
 
 import (
+	"encoding/json"
 	"sync"
 
 	"github.com/ohzqq/hare/dberr"
@@ -121,13 +122,14 @@ func (db *Database) CreateTable(tableName string) error {
 // Delete takes a table name and record id and removes that
 // record from the database.
 func (db *Database) Delete(tableName string, id int) error {
-	tbl, err := db.GetTable(tableName)
-	if err != nil {
-		return err
+	if !db.TableExists(tableName) {
+		return dberr.ErrNoTable
 	}
-	defer tbl.Close()
 
-	if err := tbl.Delete(id); err != nil {
+	db.locks[tableName].Lock()
+	defer db.locks[tableName].Unlock()
+
+	if err := db.store.DeleteRec(tableName, id); err != nil {
 		return err
 	}
 
@@ -160,38 +162,73 @@ func (db *Database) DropTable(tableName string) error {
 // implements the Record interface, finds the associated record from the
 // table, and populates the struct.
 func (db *Database) Find(tableName string, id int, rec Record) error {
-	tbl, err := db.GetTable(tableName)
+	if !db.TableExists(tableName) {
+		return dberr.ErrNoTable
+	}
+
+	db.locks[tableName].RLock()
+	defer db.locks[tableName].RUnlock()
+
+	rawRec, err := db.store.ReadRec(tableName, id)
 	if err != nil {
 		return err
 	}
-	defer tbl.Close()
 
-	return tbl.Find(id, rec)
+	err = json.Unmarshal(rawRec, rec)
+	if err != nil {
+		return err
+	}
+
+	err = rec.AfterFind(db)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // IDs takes a table name and returns a list of all record ids for
 // that table.
 func (db *Database) IDs(tableName string) ([]int, error) {
-	tbl, err := db.GetTable(tableName)
+	if !db.TableExists(tableName) {
+		return nil, dberr.ErrNoTable
+	}
+
+	db.locks[tableName].Lock()
+	defer db.locks[tableName].Unlock()
+
+	ids, err := db.store.IDs(tableName)
 	if err != nil {
 		return nil, err
 	}
-	defer tbl.Close()
 
-	return tbl.IDs()
+	return ids, err
 }
 
 // Insert takes a table name and a struct that implements the Record
 // interface and adds a new record to the table.  It returns the
 // new record's id.
 func (db *Database) Insert(tableName string, rec Record) (int, error) {
-	tbl, err := db.GetTable(tableName)
+	if !db.TableExists(tableName) {
+		return 0, dberr.ErrNoTable
+	}
+
+	db.locks[tableName].Lock()
+	defer db.locks[tableName].Unlock()
+
+	id := db.incrementLastID(tableName)
+	rec.SetID(id)
+
+	rawRec, err := json.Marshal(rec)
 	if err != nil {
 		return 0, err
 	}
-	defer tbl.Close()
 
-	return tbl.Insert(rec)
+	if err := db.store.InsertRec(tableName, id, rawRec); err != nil {
+		return 0, err
+	}
+
+	return id, nil
 }
 
 // TableExists takes a table name and returns true if the table exists,
@@ -209,11 +246,25 @@ func (db *Database) TableNames() []string {
 // interface and updates the record in the table that has that record's
 // id.
 func (db *Database) Update(tableName string, rec Record) error {
-	tbl, err := db.GetTable(tableName)
+	if !db.TableExists(tableName) {
+		return dberr.ErrNoTable
+	}
+
+	db.locks[tableName].Lock()
+	defer db.locks[tableName].Unlock()
+
+	id := rec.GetID()
+
+	rawRec, err := json.Marshal(rec)
 	if err != nil {
 		return err
 	}
-	return tbl.Update(rec)
+
+	if err := db.store.UpdateRec(tableName, id, rawRec); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // unexported methods
